b8e6f6ed2dcd3f67758e057c152656e66b53be8a
Binay <binaybehera0000@gmail.com>
2025-03-05 17:37:41 +0530

    merging successful

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', home ,name= 'home')
-=======
-    path('', home, name='home'),
->>>>>>> ffe56e946e8b819e6a49197869e42379a86883f6
 ]
